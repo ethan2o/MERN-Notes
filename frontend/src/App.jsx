b5d3a9519963ae91897e09af7032af7d7334b018
Ethan <ethan.to@student.unimelb.edu.au>
2025-11-07 19:21:39 +1100

    add page templates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Route, Routes} from "react-router"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+import toast from "react-hot-toast"
 
 const App = () => {
   return (
-    <div>
-      
+    <div data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+  return (
+    <div>
+      Details
+    </div>
+  )
+}
+
+export default NoteDetailPage
