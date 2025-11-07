fe42e3888c39b2a069cf6900870b047eb213f8e0
Ethan <ethan.to@student.unimelb.edu.au>
2025-11-07 19:34:37 +1100

    implement navbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='min-h-screen'>
+      <NavBar />
       Home
     </div>
   )
