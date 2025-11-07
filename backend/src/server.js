8d127a1d27dbef17b1812d9bb6f414aec5aa915e
Ethan <ethan.to@student.unimelb.edu.au>
2025-11-07 16:38:54 +1100

    use PORT constant

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,12 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB, config } from "./config/db.js";
 
 const app = express();
+const PORT = config.port;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(config.port, () => {
-    console.log("Server started on port", config.port);
+app.listen(PORT, () => {
+    console.log("Server started on port", PORT);
 });
