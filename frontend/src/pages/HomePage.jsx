a7641f483ed7cfe8f784d048b1a51910f0a6b848
Ethan <ethan.to@student.unimelb.edu.au>
2025-11-07 19:46:01 +1100

    implement rate limit screen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React, { useState } from 'react';
+import NavBar from '../components/NavBar';
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
   return (
     <div className='min-h-screen'>
       <NavBar />
+
+      {isRateLimited && <RateLimitedUI />}  
       Home
     </div>
   )
